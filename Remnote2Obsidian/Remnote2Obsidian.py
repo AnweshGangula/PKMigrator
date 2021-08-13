d582cd467343325b0fa24ee7dd344f0ed3a78bd5
Gangula2 <gangulaanwesh@gmail.com>
2021-08-13 10:39:21 +0530

    Organized code and created function to filter child

--- Remnote2Obsidian/Remnote2Obsidian.py
+++ Remnote2Obsidian/Remnote2Obsidian.py
@@ -6,29 +6,35 @@ import json
 
 # user-input variables: ----------------------------------------
 jsonPath = "rem.json"
-homepageID = "pAbgiAqZ45tLDzSpS"
+# jsonPath = sys.argv[1]
+# homepageName = "Personal"
+homepageID = "pAbgiAqZ45tLDzSpS" # you can find this in the URL (eg: https://www.remnote.io/document/HrxrQbMC3fXbBpWPB)
 folderName = "Rem2Obs"
 # ---------------------------------------------------------------
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-# jsonPath = sys.argv[1]
-
 Rem2ObsPath = os.path.join(dir_path, folderName)
 os.makedirs(Rem2ObsPath, exist_ok=True)
 
-
 j = json.load(open(jsonPath, mode="rt", encoding="utf-8", errors="ignore"))
-
 RemnoteDocs = j["docs"]
 
-mainPages = [x["subBlocks"] for x in RemnoteDocs if x["_id"] == homepageID][0]
-filteredPages = [x for x in RemnoteDocs if x["_id"] in mainPages]
-mainFiles = [x for x in filteredPages if not "contains:" in x["key"]]  # Rem with "contains:" in key are auto-generated
+mainPagesID = [x["subBlocks"] for x in RemnoteDocs if x["_id"] == homepageID][0]
+# print([[x["key"], x["_id"]] for x in RemnoteDocs if x["key"] == [homepageName]])
+mainPages = [x for x in RemnoteDocs if x["_id"] in mainPagesID]
 
-for file in mainFiles:
+def filterChildren(pages):
+    filteredChildren = [x for x in pages if not "contains:" in x["key"]]
+
+    return filteredChildren
+
+
+filteredPages = filterChildren(mainPages) # Rem with "contains:" in key are auto-generated
+
+for file in filteredPages:
     # print(file["key"][0])
     filename = os.path.join(Rem2ObsPath, file["key"][0] + ".md")
     with open(filename, mode="wt", encoding="utf-8") as f:
         f.write("# " + file["key"][0])
 
-print(str(len(mainFiles)) + " files generated")
+print(str(len(filteredPages)) + " files generated")
