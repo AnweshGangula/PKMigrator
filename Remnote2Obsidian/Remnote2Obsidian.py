b7f0e7f4e10aa355e991cccec92231a1714f2dfe
Gangula2 <gangulaanwesh@gmail.com>
2021-08-17 23:19:53 +0530

    python re-organizable functions + PBI minor changes

--- Remnote2Obsidian/Remnote2Obsidian.py
+++ Remnote2Obsidian/Remnote2Obsidian.py
@@ -16,13 +16,40 @@ folderName = "Rem2Obs"
 Rem2ObsPath = os.path.join(dir_path, folderName)
 os.makedirs(Rem2ObsPath, exist_ok=True)
 
-j = json.load(open(jsonPath, mode="rt", encoding="utf-8", errors="ignore"))
-RemnoteDocs = j["docs"]
+remnoteJSON = json.load(open(jsonPath, mode="rt", encoding="utf-8", errors="ignore"))
+RemnoteDocs = remnoteJSON["docs"]
 mainPageID = [x["children"] for x in RemnoteDocs if x["_id"] == homepageID][0]
 # print([[x["key"], x["_id"]] for x in RemnoteDocs if x["key"] == [homepageName]])
 
-def expandChildren(pages, ID):
-    childID = [x["children"] for x in pages if x["_id"] == ID][0]
+def main():
+    mainFiles = expandChildren(homepageID)
+
+    for file in mainFiles:
+        # print(file["key"][0])
+        filename = os.path.join(Rem2ObsPath, file["key"][0] + ".md")
+
+        child = expandChildren(file["_id"])
+        bullets = []
+        expandBullets = ""
+        for x in child:
+            bullets.append("* " + x + "\n")
+            expandBullets += "* " + x + "\n"
+        with open(filename, mode="wt", encoding="utf-8") as f:
+            f.write("# " + file["key"][0] + "\n" + expandBullets)
+        # print(f'{file["key"][0]}.md created')
+
+    print(str(len(mainFiles)) + " files generated")
+
+
+def ignoreRem(dict):
+    if((dict["key"] == []) or ("contains:" in dict["key"]) or ("rcrp" in dict) or ("type" in dict and dict["type"] == 6)):
+        return True
+    else:
+        return False
+
+
+def expandChildren(ID):
+    childID = [x["children"] for x in RemnoteDocs if x["_id"] == ID][0]
     filteredChildren = []
     text = ""
     if(len(childID) == 0):
@@ -45,16 +72,10 @@ def expandChildren(pages, ID):
                 if(len(x["children"])>0):
                     for child in x["children"]:
                         # print({"child: " + child, "Parent: " +  x["_id"]})
-                        filteredChildren.extend(expandChildren(RemnoteDocs, child))
+                        filteredChildren.extend(expandChildren(child))
 
     return filteredChildren
 
-
-def ignoreRem(dict):
-    if((dict["key"] == []) or ("contains:" in dict["key"]) or ("rcrp" in dict) or ("type" in dict and dict["type"] == 6)):
-            return True
-    else:
-        return False
 
 def dictFromID(ID):
     dict=[]
@@ -74,7 +95,7 @@ def textFromKey(key):
             newKey = dictFromID(item["_id"])["key"]
             text += f'((^{textFromKey(newKey)}))'
         elif(item["i"] == "o"):
-            text += f'```{item["language"]}\n{item["text"]}\n```'
+            text += f'```{item["language"]}\n{item["text"]}\n  ```'
         elif("q" in item and item["q"]):
             text += f'`{item["text"]}`'
         elif(item["i"] == "m" and "url" in item):
@@ -86,20 +107,5 @@ def textFromKey(key):
     return text
 
 
-mainFiles = expandChildren(RemnoteDocs, homepageID)
-
-for file in mainFiles:
-    # print(file["key"][0])
-    filename = os.path.join(Rem2ObsPath, file["key"][0] + ".md")
-
-    child = expandChildren(RemnoteDocs, file["_id"])
-    bullets = []
-    expandBullets = ""
-    for x in child:
-        bullets.append("* " + x + "\n")
-        expandBullets += "* " + x + "\n"
-    with open(filename, mode="wt", encoding="utf-8") as f:
-        f.write("# " + file["key"][0] + "\n" + expandBullets)
-    # print(f'{file["key"][0]}.md created')
-
-print(str(len(mainFiles)) + " files generated")
+if __name__ == '__main__':
+    main()
