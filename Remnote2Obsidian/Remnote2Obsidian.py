d413b7955158fd011e4d42553285cc3745fa0d0a
Gangula2 <gangulaanwesh@gmail.com>
2022-10-28 18:05:17 +0530

    VS Code formatting

--- Remnote2Obsidian/Remnote2Obsidian.py
+++ Remnote2Obsidian/Remnote2Obsidian.py
@@ -4,7 +4,7 @@
 import sys, os, json, datetime, re
 
 # Import modules from current project:
-from progressBar import printProgressBar 
+from progressBar import printProgressBar
 
 # Custom Package installation
 from dateutil.parser import parse as dateParse
@@ -19,16 +19,18 @@ RemLanguages = "../Data/RemLanguages.json"
 langJsonPath = os.path.join(dir_path, RemLanguages)
 vaultName = "Rem2Obs"
 dailyDocsFolder = "Daily Documents"
-indent = "\t" # "    " # choose how are lines indented (tab ("\t") vs 4spaces("    ")) --> https://forum.obsidian.md/t/meta-post-common-css-hacks/1978/509?u=gangula
-highlightToHTML = True # if False: Highlights will be '==sampleText==', if True '<mark style=" background-color: {color}; ">{text}</mark>'
+indent = "\t"  # "    " # choose how are lines indented (tab ("\t") vs 4spaces("    ")) --> https://forum.obsidian.md/t/meta-post-common-css-hacks/1978/509?u=gangula
+highlightToHTML = True  # if False: Highlights will be '==sampleText==', if True '<mark style=" background-color: {color}; ">{text}</mark>'
 previewBlockRef = True
-delimiterSR = " -- " # Spaced Repetition Delimiter
+delimiterSR = " -- "  # Spaced Repetition Delimiter
 
 re_HTML = re.compile("(?<!`)<(?!\s|-).+?>(?!`)")
-re_newLine = re.compile("(\\n){3,}") # replace more than 2 newlines with only 2: https://regex101.com/r/9VAqaO/1/
-re_remID = re.compile(r'\[\[')
+re_newLine = re.compile(
+    "(\\n){3,}"
+)  # replace more than 2 newlines with only 2: https://regex101.com/r/9VAqaO/1/
+re_remID = re.compile(r"\[\[")
 # ---------------------------------------------------------------
-pbr=""
+pbr = ""
 if previewBlockRef:
     pbr = "!"
 
@@ -47,12 +49,14 @@ allParentRem = []
 # allFolders = []
 # topFolders = []
 for x in RemnoteDocs:
-    if(x.get("n", False) == 1 and
-     x.get("_id", False) not in ignoreID and
-     x["key"] != [] and
-     x["key"][0] not in ignoreKey):
+    if (
+        x.get("n", False) == 1
+        and x.get("_id", False) not in ignoreID
+        and x["key"] != []
+        and x["key"][0] not in ignoreKey
+    ):
         allParentRem.append(x)
-        if(x.get("rcrt", False) == "d"):
+        if x.get("rcrt", False) == "d":
             # Convert Daily Documents to folder
             x["key"][0] = dailyDocsFolder
             x["forceIsFolder"] = True
@@ -68,18 +72,21 @@ def getAllDocs(RemList):
         if rem.get("forceIsFolder", False):
             childRem = []
             for child in rem["ch"]:
-                dict = [x for x in RemnoteDocs if x["_id"] == child][0] 
+                dict = [x for x in RemnoteDocs if x["_id"] == child][0]
                 childRem.append(dict)
             IDlist.extend(getAllDocs(childRem))
-        if(len(rem.get("ch", []))>0
-        or (len(rem.get("portalsIn", []))>0)
-        or (len(rem.get("references", []))>0)
-        or (len(rem.get("typeChildren", []))>0)):
+        if (
+            len(rem.get("ch", [])) > 0
+            or (len(rem.get("portalsIn", [])) > 0)
+            or (len(rem.get("references", [])) > 0)
+            or (len(rem.get("typeChildren", [])) > 0)
+        ):
             IDlist.append(rem["_id"])
         else:
             # print("REM not used anywhere")
             pass
     return IDlist
+
 
 allDocID = getAllDocs(allParentRem)
 allDocID = list(set(allDocID) - set(ignoreID))
@@ -87,20 +94,30 @@ allDocID = list(set(allDocID) - set(ignoreID))
 
 created = []
 notCreated = []
+
+
 def main():
-    printProgressBar(0, len(allParentRem), prefix = 'Progress:', suffix = 'Complete', length = 50)
-    i=0
+    printProgressBar(
+        0, len(allParentRem), prefix="Progress:", suffix="Complete", length=50
+    )
+    i = 0
     for dict in allParentRem:
         i += 1
         if ignoreRem(dict["_id"]):
             continue
         createFile(dict["_id"], Rem2ObsPath)
-        printProgressBar(i, len(allParentRem), prefix = 'Progress:', suffix = 'Complete', length = 50)
+        printProgressBar(
+            i, len(allParentRem), prefix="Progress:", suffix="Complete", length=50
+        )
 
     timetaken = str(datetime.datetime.now() - start_time)
     print(f"\nTime Taken to Generate '{vaultName}' Obsidian Vault: {timetaken}")
     print("\n" + str(len(created)) + " files generated")
-    print(str(len(notCreated)) + " file/s listed below could not be generated\n" + "\n".join(notCreated)) if len(notCreated)>0 else None
+    print(
+        str(len(notCreated))
+        + " file/s listed below could not be generated\n"
+        + "\n".join(notCreated)
+    ) if len(notCreated) > 0 else None
 
 
 def createFile(remID, remFolderPath):
@@ -114,9 +131,9 @@ def createFile(remID, remFolderPath):
     filename = textSplit[0]
     filename = replaceRemID(filename)
     fileDesc = ""
-    if len(textSplit)>1:
+    if len(textSplit) > 1:
         fileDesc = "\nFile Description: " + textSplit[1]
-    
+
     if remDict.get("forceIsFolder", False):
         newFilePath = os.path.join(remFolderPath, filename)
         for child in remDict["ch"]:
@@ -125,7 +142,7 @@ def createFile(remID, remFolderPath):
         os.makedirs(remFolderPath, exist_ok=True)
         fileTitle = filename
         # filename = re.sub('[^\w\-_\. ]', '_', filename)
-        if(os.path.basename(remFolderPath) == dailyDocsFolder):
+        if os.path.basename(remFolderPath) == dailyDocsFolder:
             # dailyDocName = datetime.datetime.strptime(filename, "%B %dth, %Y").date()
             try:
                 dailyDocName = dateParse(filename)
@@ -138,7 +155,7 @@ def createFile(remID, remFolderPath):
         try:
             with open(filePath, mode="wt", encoding="utf-8") as f:
                 child = expandChildren(remID)
-                fileMetadata = f'# '
+                fileMetadata = f"# "
                 expandBullets = "\n".join(child)
 
                 f.write(fileMetadata + fileTitle + fileDesc + "\n\n" + expandBullets)
@@ -153,13 +170,15 @@ def createFile(remID, remFolderPath):
 def ignoreRem(ID):
     # TODO: add more ignore ID's
     dict = dictFromID(ID)
-    if(dict == []
-    or dict["key"] == [] 
-    or ("contains:" in dict["key"]) 
-    or ("rcrp" in dict) 
-    or ("rcrs" in dict) 
-    or ("rcrt" in dict and dict.get("rcrt") != "c" and dict.get("rcrt") != "d")
-    or (dict.get("type", False) == 6)):
+    if (
+        dict == []
+        or dict["key"] == []
+        or ("contains:" in dict["key"])
+        or ("rcrp" in dict)
+        or ("rcrs" in dict)
+        or ("rcrt" in dict and dict.get("rcrt") != "c" and dict.get("rcrt") != "d")
+        or (dict.get("type", False) == 6)
+    ):
         return True
     else:
         return False
@@ -191,13 +210,13 @@ def expandChildren(ID, level=0):
                 text = text.replace("\n", "\n" + blankPrefix)
             filteredChildren.append(text)
 
-            filteredChildren.extend(expandChildren(childID, level = level + 1 ))
+            filteredChildren.extend(expandChildren(childID, level=level + 1))
 
     return filteredChildren
 
 
 def dictFromID(ID):
-    dict=[]
+    dict = []
     try:
         dict = [x for x in RemnoteDocs if x["_id"] == ID][0]
     except Exception as e:
@@ -206,7 +225,8 @@ def dictFromID(ID):
         pass
     return dict
 
-def textFromID(ID, level = 0):
+
+def textFromID(ID, level=0):
     text = ""
     if ignoreRem(ID):
         return text
@@ -226,57 +246,60 @@ def textFromID(ID, level = 0):
     #     text += delimiterSR + arrayToText(value, ID)
     if level == 0:
         # level is used to disable recursive expansion, since tags don't need to be recursive
-        if ((len(dict.get("typeParents", []))>0) 
-        and not ID in allDocID 
-        and not(dict.get("forceIsFolder", False))):
+        if (
+            (len(dict.get("typeParents", [])) > 0)
+            and not ID in allDocID
+            and not (dict.get("forceIsFolder", False))
+        ):
             text += convertTags(dict)
-    
+
     if text.startswith("```"):
         text = text.replace("\r\n", "\n")
         # in Windows - "\r\n" means end of line - https://stackoverflow.com/a/1761086/6908282
-    
+
     return text
+
 
 def arrayToText(array, ID):
     text = ""
     for item in array:
-        if(isinstance(item, str)):
+        if isinstance(item, str):
             text += fence_HTMLtags(item)
-        elif(item["i"] == "q" and "_id" in item):
+        elif item["i"] == "q" and "_id" in item:
             newDict = dictFromID(item["_id"])
             if newDict == []:
                 continue
             newID = newDict["_id"]
             parentPath = parentFromID(newID)
             if newID in allDocID:
-                text += f'[[{parentPath}]]'
+                text += f"[[{parentPath}]]"
             else:
-                text += f'{pbr}[[{parentPath}#^{newID}]]'
-        elif(item["i"] == "o"):
+                text += f"{pbr}[[{parentPath}#^{newID}]]"
+        elif item["i"] == "o":
             text += f'```{getOrgLanguage(item.get("language", "None"))}\n{item["text"]}\n```'
-        elif(item["i"] == "i" and "url" in item):
+        elif item["i"] == "i" and "url" in item:
             text += f'![]({item["url"]})'
-        elif(item["i"] == "m"):
+        elif item["i"] == "m":
             currText = item["text"]
             currText = fence_HTMLtags(currText)
-            if ("url" in item):
+            if "url" in item:
                 text += f'[{currText.strip()}]({item["url"]})'
-            elif (currText.strip() == ""):
+            elif currText.strip() == "":
                 text += currText
-            elif(item.get("q", False)):
-                text += f'`{currText}`'
-            elif(item.get("x", False)):
-                text += f'$${currText}$$'
-            elif(item.get("b", False)):
-                if(item.get("h", False)):
-                    text += textHighlight(currText, item["h"], html = highlightToHTML)
+            elif item.get("q", False):
+                text += f"`{currText}`"
+            elif item.get("x", False):
+                text += f"$${currText}$$"
+            elif item.get("b", False):
+                if item.get("h", False):
+                    text += textHighlight(currText, item["h"], html=highlightToHTML)
                 else:
-                    text += f'**{currText}**'
-            elif(item.get("h", False)):
-                text += textHighlight(currText, item["h"], html = highlightToHTML)
-            elif(item.get("u", False)):
+                    text += f"**{currText}**"
+            elif item.get("h", False):
+                text += textHighlight(currText, item["h"], html=highlightToHTML)
+            elif item.get("u", False):
                 text += currText
-        elif(item["i"] == "q" and "textOfDeletedRem" in item):
+        elif item["i"] == "q" and "textOfDeletedRem" in item:
             text += "#DeletedRem: " + "".join(item["textOfDeletedRem"])
         else:
             print("Could not Extract text at textFromID function for ID: " + ID)
@@ -285,9 +308,9 @@ def arrayToText(array, ID):
 
 
 def replaceRemID(text):
-    text = re.sub(re_remID, ' #', text)
+    text = re.sub(re_remID, " #", text)
     text = text.replace("]]", "")
-    text = text.replace("/", "") # replace "/" if added in parentFromID() function
+    text = text.replace("/", "")  # replace "/" if added in parentFromID() function
     text = text.strip()
 
     return text
@@ -297,41 +320,41 @@ def convertTags(dict):
     text = ""
     for x in dict["typeParents"]:
         if not ignoreRem(x):
-            textExtract = textFromID(x, level = 1).strip()
-            textExtract = re.sub(r'[^A-Za-z0-9-]+', '_', textExtract)
-            text += f' #{textExtract}'
-        
+            textExtract = textFromID(x, level=1).strip()
+            textExtract = re.sub(r"[^A-Za-z0-9-]+", "_", textExtract)
+            text += f" #{textExtract}"
+
     return text
 
 
-def textHighlight(text, colorNum, html = False):
+def textHighlight(text, colorNum, html=False):
     if html:
         # Switch-Case: https://stackoverflow.com/a/60211/6908282
         def switch(x):
             colorList = {
-                1 : "firebrick",
-                2 : "darkorange",
-                3 : "goldenrod",
-                4 : "seagreen",
-                5 : "rebeccapurple",
-                6 : "steelblue",
+                1: "firebrick",
+                2: "darkorange",
+                3: "goldenrod",
+                4: "seagreen",
+                5: "rebeccapurple",
+                6: "steelblue",
             }
-            
+
             color = colorList.get(x, "")
             return color
 
         color = switch(colorNum)
         text = f'<mark style=" background-color: {color}; ">{text}</mark>'
     else:
-        text = f'=={text}=='
-    
+        text = f"=={text}=="
+
     return text
 
 
 def getTODO(keyDict):
     if isinstance(keyDict["key"][0], dict) and keyDict["key"][0].get("i") == "o":
         # Excludue "Custom CSS" Rem - Dont add Todo check-boxes from here
-        # Typically, CSS code-block has only one item in the key dictionary and all code-block have property `"i": "o"` 
+        # Typically, CSS code-block has only one item in the key dictionary and all code-block have property `"i": "o"`
         return False
     todo = keyDict.get("crt")
     if todo and "t" in todo:
@@ -339,6 +362,7 @@ def getTODO(keyDict):
         return todoStatus
     else:
         return False
+
 
 def fence_HTMLtags(string):
     # Reference: https://regex101.com/r/BVWwGK/10
@@ -355,7 +379,7 @@ def parentFromID(ID):
     if ignoreRem(ID):
         return fileName
     dict = dictFromID(ID)
-    if(ID in allDocID):
+    if ID in allDocID:
         filePath = getFilePath(ID)
         filePath.reverse()
         fileName = "/".join(filePath) + "/" + textFromID(ID)
@@ -374,9 +398,12 @@ def getFilePath(ID):
 
     return pathList
 
+
 def getOrgLanguage(lang):
     lang = lang.lower()
-    langList = json.load(open(langJsonPath, mode="rt", encoding="utf-8", errors="ignore"))
+    langList = json.load(
+        open(langJsonPath, mode="rt", encoding="utf-8", errors="ignore")
+    )
     try:
         identifier = langList[lang]
     except Exception as e:
@@ -386,5 +413,6 @@ def getOrgLanguage(lang):
 
     return identifier
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
